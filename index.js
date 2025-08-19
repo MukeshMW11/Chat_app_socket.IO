233d536aab613e775b90b9f87e9441868c111fb2
unknown <mukeshbhattampb111@gmail.com>
2025-08-19 15:29:14 +0545

    feat:socket integration

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ import express from "express";
 import http from "http";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import { Server } from "socket.io";
 
 const app = express();
 
 const server = http.createServer(app);
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
-  res.sendFile(join(__dirname,'index.html'));
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-app.listen(3000, () => {
+
+
+io.on('connection',(socket)=>{
+console.log('Web Socket connected Successfully.')
+socket.on('disconnect',()=>{
+    console.log('Sokcet Disconnected.')
+})
+})
+
+server.listen(3000, () => {
   console.log("The server is listening at port 3000");
 });
